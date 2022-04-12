e0cf110071ec807c25180bc07ea848921e07eb58
sasugadayo <sasugaff@icloud.com>
2022-04-12 22:19:27 +0900

    blog API出力

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,28 @@
 <template>
-  <Tutorial/>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'IndexPage'
-}
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      "https://htw7b0y0oi.microcms.io/api/v1/blog",
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: {
+          "X-MICROCMS-API-KEY": "9e9dfe45d78c4190b56c69c0684fbe77fdb1",
+        },
+      }
+    );
+    return data;
+  },
+};
 </script>
