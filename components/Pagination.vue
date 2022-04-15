fcdfcb3f9b311aeed39514cec24863ac7ef19caf
sasugadayo <sasugaff@icloud.com>
2022-04-15 21:20:03 +0900

    pagenations for each categories

---
+++ components/Pagination.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="wrapper">
+    <ul class="pager">
+      <li v-if="current > 1" class="page arrow">
+        <nuxt-link :to="getPath(current - 1)">
+          <img src="/images/icon_arrow_left.svg" alt="前のページへ" />
+        </nuxt-link>
+      </li>
+      <li v-if="3 < current" class="page">
+        <nuxt-link :to="getPath(1)"> 1 </nuxt-link>
+      </li>
+      <li v-if="4 < current" class="omission">...</li>
+      <li
+        v-for="p in pager"
+        v-show="current - 3 <= p && p <= current + 1"
+        :key="p"
+        class="page"
+        :class="{ active: current === p + 1 }"
+      >
+        <nuxt-link :to="getPath(p + 1)">
+          {{ p + 1 }}
+        </nuxt-link>
+      </li>
+      <li v-if="current + 3 < pager.length" class="omission">...</li>
+      <li v-if="current + 2 < pager.length" class="page">
+        <nuxt-link :to="getPath(pager.length)">
+          {{ pager.length }}
+        </nuxt-link>
+      </li>
+      <li v-if="current < pager.length" class="page arrow">
+        <nuxt-link :to="getPath(current + 1)">
+          <img src="/images/icon_arrow_right.svg" alt="次のページへ" />
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pager: {
+      type: Array,
+      required: false,
+      default: () => [],
+    },
+    current: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: Object,
+      required: false,
+      default: undefined,
+    },
+  },
+  methods: {
+    getPath(p) {
+      return `/${
+        this.category !== undefined ? `category/${this.category.id}/` : ""
+      }page/${p}`;
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,92 @@
 <template>
-  <ul>
-    <li v-for="content in contents" :key="content.id">
-      <nuxt-link :to="`/${content.id}`">{{ content.title }}</nuxt-link>
-    </li>
-  </ul>
+  <div>
+    <ul>
+      <li v-for="content in contents" :key="content.id">
+        <nuxt-link :to="`/${content.id}`">{{ content.title }}</nuxt-link>
+      </li>
+    </ul>
+    <Pagination
+      :pager="pager"
+      :current="Number(page)"
+      :category="selectedCategory"
+    />
+  </div>
 </template>
+<script>
+// import axios from "axios";
+// export default {
+//   async asyncData({ params }) {
+//     const page = params.p || "1";
+//     const limit = 10;
+//     const { data } = await axios.get(
+//       `https://htw7b0y0oi.microcms.io/api/v1/blog?limit=${limit}&offset=${
+//         (page - 1) * limit
+//       }`,
+//       {
+//         headers: {
+//           "X-MICROCMS-API-KEY": "9e9dfe45d78c4190b56c69c0684fbe77fdb1",
+//         },
+//       }
+//     );
+//     return data;
+//   },
+// };
+</script>
+
 <script>
 import axios from "axios";
 export default {
+  // async asyncData({ params }) {
+  //   const page = params.p || "1";
+  //   const categoryId = params.categoryId;
+  //   const limit = 10;
+  //   const { data } = await axios.get(
+  //     `https://htw7b0y0oi.microcms.io/api/v1/blog?limit=${limit}${
+  //       categoryId === undefined ? "" : `&filters=category[equals]${categoryId}`
+  //     }&offset=${(page - 1) * limit}`,
+  //     {
+  //       headers: {
+  //         "X-MICROCMS-API-KEY": "9e9dfe45d78c4190b56c69c0684fbe77fdb1",
+  //       },
+  //     }
+  //   );
+  //   return data;
+  // },
+
   async asyncData({ params }) {
     const page = params.p || "1";
+    const categoryId = params.categoryId;
     const limit = 10;
+
     const { data } = await axios.get(
-      `https://htw7b0y0oi.microcms.io/api/v1/blog?limit=${limit}&offset=${
-        (page - 1) * limit
-      }`,
+      `https://htw7b0y0oi.microcms.io/api/v1/blog?limit=${limit}${
+        categoryId === undefined ? "" : `&filters=category[equals]${categoryId}`
+      }&offset=${(page - 1) * limit}`,
       {
         headers: {
           "X-MICROCMS-API-KEY": "9e9dfe45d78c4190b56c69c0684fbe77fdb1",
         },
       }
     );
-    return data;
+    const categories = await axios.get(
+      `https://htw7b0y0oi.microcms.io/api/v1/categories?limit=100`,
+      {
+        headers: {
+          "X-MICROCMS-API-KEY": "9e9dfe45d78c4190b56c69c0684fbe77fdb1",
+        },
+      }
+    );
+    const selectedCategory =
+      categoryId !== undefined
+        ? categories.data.contents.find((content) => content.id === categoryId)
+        : undefined;
+
+    return {
+      ...data,
+      selectedCategory,
+      page,
+      pager: [...Array(Math.ceil(data.totalCount / limit)).keys()],
+    };
   },
 };
 </script>
